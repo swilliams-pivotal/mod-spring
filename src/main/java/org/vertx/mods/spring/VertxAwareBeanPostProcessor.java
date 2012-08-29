ce1cecd0865f6d4abd92713c3951aadd011e7f48
Stuart Williams <swilliams@vmware.com>
2012-08-29 01:35:39 +0100

    add another javadoc note

--- src/main/java/org/vertx/mods/spring/VertxAwareBeanPostProcessor.java
+++ src/main/java/org/vertx/mods/spring/VertxAwareBeanPostProcessor.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.vertx.mods.spring;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.vertx.java.core.Vertx;
 
+/**
+ * @author swilliams
+ * @since 1.0
+ */
 public class VertxAwareBeanPostProcessor implements BeanPostProcessor {
 
   private Vertx vertx;
@@ -12,6 +31,9 @@ public class VertxAwareBeanPostProcessor implements BeanPostProcessor {
     this.vertx = vertx;
   }
 
+  /**
+   * Detects beans that implement VertxAware.
+   */
   @Override
   public Object postProcessBeforeInitialization(Object bean, String beanName)
       throws BeansException {
@@ -25,10 +47,12 @@ public class VertxAwareBeanPostProcessor implements BeanPostProcessor {
     return bean;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Object postProcessAfterInitialization(Object bean, String beanName)
       throws BeansException {
-
     return bean;
   }
 
